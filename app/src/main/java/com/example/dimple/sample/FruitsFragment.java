51035bc34e9aa9c4413d8f86c4eaace905f1c17d
Dimple Maniar <dimple@Dimples-MacBook-Pro.local>
2018-06-04 12:28:02 -0700

    first commmit

---
+++ app/src/main/java/com/example/dimple/sample/FruitsFragment.java
@@ -0,0 +1,40 @@
+package com.example.dimple.sample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FruitsFragment extends Fragment {
+
+    private String fruits[] = {"apple", "banana"};
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fruits_fragment, container, false);
+        ListView lv = (ListView) rootView.findViewById(R.id.fruitsListView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), R.layout.fruits_fragment, fruits);
+        lv.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private String[] getFruits(){
+        return fruits;
+
+    }
+
+    @Override
+    public String toString() {
+        String title = "Fruits";
+        return title;
+    }
+}
---
+++ app/src/main/java/com/example/dimple/sample/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.dimple.sample;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+
+    private ViewPager mViewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        mViewPager = (ViewPager) findViewById(R.id.mViewPager_ID);
+        this.addPages();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        tabLayout= (TabLayout) findViewById(R.id.mTab_ID);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.setOnTabSelectedListener(this);
+
+    }
+
+    private void addPages()
+    {
+        MyPagerAdapter pagerAdapter=new MyPagerAdapter(this.getSupportFragmentManager());
+        pagerAdapter.addFragment(new FruitsFragment());
+
+
+        //SET ADAPTER TO VP
+        mViewPager.setAdapter(pagerAdapter);
+    }
+
+
+
+
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        mViewPager.setCurrentItem(tab.getPosition());
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+}
