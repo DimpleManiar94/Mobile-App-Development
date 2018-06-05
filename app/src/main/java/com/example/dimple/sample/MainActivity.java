3b1696d9e0602659f4317dc9cee868ccf0f42dbc
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-06-04 22:17:10 -0700

    Fetch filtered data from server
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- app/src/main/java/com/example/dimple/sample/MainActivity.java
+++ app/src/main/java/com/example/dimple/sample/MainActivity.java
@@ -19,6 +19,8 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
 
 
